c77097584d1abcb9eef7e5724598c4aa004c5a30
Alex Nastin <aliaksei.nastsin@netcracker.com>
2019-04-15 16:39:06 +0300

    feat: new

--- src/main/java/com/arpa/csv/generator/Main.java
+++ src/main/java/com/arpa/csv/generator/Main.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
+    //    /home/netcrk/minio/data/csv/ 9000 1000 3000000
     private static final String FILE_PREFIX = "csv";
     private static final String FILE_POSTFIX = ".csv";
     private static final String SEPARATOR = ",";
@@ -14,7 +15,7 @@ public class Main {
     private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
-        if (args.length != 3) {
+        if (args.length != 4) {
             System.out.println("Incorrect argument number. Should be 3 (path, number of files, number of records in each file) but found " + args.length);
             return;
         }
@@ -22,39 +23,34 @@ public class Main {
         String path = args[0];
         long fileNumber = Long.parseLong(args[1]);
         long recordsNumber = Long.parseLong(args[2]);
+        int ids = Integer.parseInt(args[3]);
 
         ExecutorService executorService = Executors.newFixedThreadPool(5);
 
         for (long fileIndex = 0; fileIndex < fileNumber; fileIndex++) {
             final long stableFileIndex = fileIndex;
-            executorService.submit(() -> createCsvFile(path, stableFileIndex, recordsNumber));
+            executorService.submit(() -> createCsvFile(path, stableFileIndex, recordsNumber, ids));
         }
-
         executorService.shutdown();
     }
 
-    private static void createCsvFile(String path, long fileIndex, long recordsNumber) {
+    private static void createCsvFile(String path, long fileIndex, long recordsNumber, int ids) {
         final String fullPath = path + FILE_PREFIX + fileIndex + FILE_POSTFIX;
 
         try (FileOutputStream fileOutputStream = new FileOutputStream(fullPath)) {
             for (long recordIndex = 0; recordIndex < recordsNumber; recordIndex++) {
-                fileOutputStream.write(getContent().getBytes());
+                fileOutputStream.write(getContent(ids).getBytes());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    private static String getContent() {
-        return getRand10000() + SEPARATOR + 1 + SEPARATOR + 1+ END_LINE;
+    private static String getContent(int ids) {
+        return getRand(ids) + SEPARATOR + 1 + SEPARATOR + 1 + END_LINE;
     }
 
-    private static int getRand100() {
-        return RANDOM.nextInt(100) + 1;
-    }
-
-    private static int getRand10000() {
-        return RANDOM.nextInt(3000000) + 1;
+    private static int getRand(int ids) {
+        return RANDOM.nextInt(ids) + 1;
     }
 }
